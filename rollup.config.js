11a6a937ac31d042f906134c92e21daf663d6565
Diwaker Singh <diwaker.surya@gmail.com>
2019-10-02 19:54:19 +0530

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH; 
 
 export default {
 	input: 'src/main.js',
